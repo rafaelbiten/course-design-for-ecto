f7b3afb1f3ce0bd5a48f6fb26ad4e1a5f7c7a22d
rafaelbiten <rafaelbiten@icloud.com>
2022-05-30 21:31:18 -0400

    [FEAT] Install initial dependencies + aliases

--- tome/mix.exs
+++ tome/mix.exs
@@ -7,7 +7,8 @@ defmodule Tome.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -22,8 +23,18 @@ defmodule Tome.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
+      {:ecto_sql, "~> 3.8"},
+      {:postgrex, "~> 0.16.3"}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp aliases do
+    [
+      get: ["deps.get", "deps.compile"],
+      check: ["dialyzer  --format dialyxir", "credo --strict"]
     ]
   end
 end
