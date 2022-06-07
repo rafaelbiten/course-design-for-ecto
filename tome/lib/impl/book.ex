5e8473b979392acd5115d8a300bdc4edea2272cd
rafaelbiten <rafaelbiten@icloud.com>
2022-06-06 20:29:55 -0400

    [FEAT] Extra validations in book update changeset

--- tome/lib/impl/book.ex
+++ tome/lib/impl/book.ex
@@ -21,11 +21,20 @@ defmodule Tome.Library.Book do
     |> cast(params, ~w[title isbn description status published_on]a)
     |> validate_required(~w[title isbn status]a)
     |> validate_inclusion(:status, ~w[working published beta retired])
+    |> unique_constraint(:isbn)
   end
 
-  def update_changeset(book, params \\ %{}) do
+  def update_changeset(%__MODULE__{} = book, params \\ %{}) do
     book
     |> cast(params, ~w[title description status published_on]a)
+    |> validate_required(~w[title isbn status]a)
     |> validate_inclusion(:status, ~w[published beta retired])
+    |> validate_published_on()
   end
+
+  #  Implementation
+
+  defp validate_published_on(changeset), do: require_published_on(changeset, book_status: get_field(changeset, :status))
+  defp require_published_on(changeset, book_status: "published"), do: validate_required(changeset, [:published_on])
+  defp require_published_on(changeset, book_status: _), do: changeset
 end
