4650c0477911c7ece47240ed3bdd52ae7187f9ef
rafaelbiten <rafaelbiten@icloud.com>
2022-06-14 22:44:07 -0400

    [FIX/FEAT] Fix seeding file + seed fake books

--- tome/priv/repo/seeds/books.exs
+++ tome/priv/repo/seeds/books.exs
@@ -1,18 +1,21 @@
 # Usage
 # mix run priv/repo/seeds/books.exs
 
-Tome.Repo.delete_all(Tome.Library.Book)
+Tome.Repo.delete_all(Tome.Core.Book)
 
 now = NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
 
-raw_books = [
+# Insert some real books
+
+real_books = [
   {"Designing Elixir Systems with OTP", "9781680506617", "Learn some OTP", "published", ~D[2019-12-01]},
   {"Progamming Ecto", "9781680502824", "Learn some Ecto", "published", ~D[2019-04-01]},
   {"Progamming Phoenix LiveView", "9781680508215", "Learn some LiveView", "beta", nil}
 ]
 
-books =
-  Enum.map(raw_books, fn {title, isbn, description, status, published_on} ->
+Tome.Repo.insert_all(
+  Tome.Core.Book,
+  Enum.map(real_books, fn {title, isbn, description, status, published_on} ->
     %{
       title: title,
       isbn: isbn,
@@ -23,5 +26,27 @@ books =
       updated_at: now
     }
   end)
+)
 
-Tome.Repo.insert_all(Tome.Library.Book, books)
+# Insert some fake books
+
+statuses = ~w[working published beta retired]
+
+Tome.Repo.insert_all(
+  Tome.Core.Book,
+  Enum.map(1..30, fn n ->
+    status = Enum.random(statuses)
+    published_on = if status in ~w[published retired], do: Date.add(Date.utc_today(), -n), else: nil
+    isbn = "1234567891234" |> String.split("", trim: true) |> Enum.shuffle() |> Enum.join()
+
+    %{
+      isbn: isbn,
+      title: "Book title #{n}",
+      description: "Book description #{n}",
+      status: status,
+      published_on: published_on,
+      inserted_at: now,
+      updated_at: now
+    }
+  end)
+)
