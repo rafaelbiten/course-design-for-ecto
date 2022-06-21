da1c5655c0b0eeaf23fba72dccd967a972f8fa90
rafaelbiten <rafaelbiten@icloud.com>
2022-06-20 21:57:50 -0400

    [CHORE] Seeding books with reviews

--- tome/priv/repo/seeds/books.exs
+++ tome/priv/repo/seeds/books.exs
@@ -1,52 +1,72 @@
 # Usage
 # mix run priv/repo/seeds/books.exs
 
-Tome.Repo.delete_all(Tome.Library.Book)
+defmodule Seed do
+  alias Tome.Repo
+  alias Tome.Library.Book
+  alias Tome.Feedback.Review
 
-now = NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
+  @now NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
 
-# Insert some real books
+  def books() do
+    # Insert some real books
+    real_books = [
+      {"Designing Elixir Systems with OTP", "9781680506617", "Learn some OTP", :published, ~D[2019-12-01]},
+      {"Progamming Ecto", "9781680502824", "Learn some Ecto", :published, ~D[2019-04-01]},
+      {"Progamming Phoenix LiveView", "9781680508215", "Learn some LiveView", :beta, nil}
+    ]
 
-real_books = [
-  {"Designing Elixir Systems with OTP", "9781680506617", "Learn some OTP", :published, ~D[2019-12-01]},
-  {"Progamming Ecto", "9781680502824", "Learn some Ecto", :published, ~D[2019-04-01]},
-  {"Progamming Phoenix LiveView", "9781680508215", "Learn some LiveView", :beta, nil}
-]
+    Enum.each(real_books, fn {title, isbn, description, status, published_on} ->
+      Repo.insert!(%Book{
+        title: title,
+        isbn: isbn,
+        description: description,
+        status: status,
+        published_on: published_on,
+        inserted_at: @now,
+        updated_at: @now
+      })
+    end)
 
-Tome.Repo.insert_all(
-  Tome.Library.Book,
-  Enum.map(real_books, fn {title, isbn, description, status, published_on} ->
-    %{
-      title: title,
-      isbn: isbn,
-      description: description,
-      status: status,
-      published_on: published_on,
-      inserted_at: now,
-      updated_at: now
-    }
-  end)
-)
+    # Insert some fake books
+    statuses = ~w[working published beta retired]a
 
-# Insert some fake books
+    Enum.each(1..30, fn n ->
+      status = Enum.random(statuses)
+      published_on = if status in ~w[published retired]a, do: Date.add(Date.utc_today(), -n), else: nil
+      isbn = "1234567891234" |> String.split("", trim: true) |> Enum.shuffle() |> Enum.join()
 
-statuses = ~w[working published beta retired]a
+      %Book{
+        isbn: isbn,
+        title: "Book title #{n}",
+        description: "Book description #{n}",
+        status: status,
+        published_on: published_on,
+        inserted_at: @now,
+        updated_at: @now
+      }
+      |> Repo.insert!()
+      |> maybe_add_review()
+    end)
+  end
 
-Tome.Repo.insert_all(
-  Tome.Library.Book,
-  Enum.map(1..30, fn n ->
-    status = Enum.random(statuses)
-    published_on = if status in ~w[published retired]a, do: Date.add(Date.utc_today(), -n), else: nil
-    isbn = "1234567891234" |> String.split("", trim: true) |> Enum.shuffle() |> Enum.join()
+  defp maybe_add_review(inserted_book) do
+    if Enum.random([true, false]) do
+      Repo.insert(%Review{
+        stars: :random.uniform(5),
+        book_id: inserted_book.id,
+        inserted_at: @now,
+        updated_at: @now
+      })
 
-    %{
-      isbn: isbn,
-      title: "Book title #{n}",
-      description: "Book description #{n}",
-      status: status,
-      published_on: published_on,
-      inserted_at: now,
-      updated_at: now
-    }
-  end)
-)
+      maybe_add_review(inserted_book)
+    end
+  end
+end
+
+# Seed the Database
+
+Tome.Repo.delete_all(Book)
+Tome.Repo.delete_all(Review)
+
+Seed.books()
