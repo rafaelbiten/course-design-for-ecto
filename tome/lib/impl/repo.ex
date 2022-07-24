6885823352e6686ea7de12f4a92c9e6cde1e71d8
rafaelbiten <rafaelbiten@icloud.com>
2022-07-24 17:15:37 -0400

    [FEAT] New fn to truncate tables and reset indexes

--- tome/lib/impl/repo.ex
+++ tome/lib/impl/repo.ex
@@ -2,4 +2,11 @@ defmodule Tome.Repo do
   use Ecto.Repo,
     otp_app: :tome,
     adapter: Ecto.Adapters.Postgres
+
+  if Mix.env() in [:dev, :test] do
+    def truncate(schema) do
+      table_name = schema.__schema__(:source)
+      query("TRUNCATE TABLE #{table_name} RESTART IDENTITY CASCADE")
+    end
+  end
 end
